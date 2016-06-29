fc759b61cd12485db6181bfc40a61ed54fda9e00
antonhansel <antoninribeaud@gmail.com>
2016-06-29 11:35:49 -0700

    express is pretty bad

--- app.js
+++ app.js
@@ -4,19 +4,18 @@
 // get all the tools we need
 var config = require('./config');
 var morgan = require('morgan');
-var path = require('path');
 var express = require('express');
 var app = express.createServer();
 var handlers = require('./lib').handlers;
+var mongoose = require('mongoose');
+// Connect to mongoose
+mongoose.connect(config.mongoUrl);
+
 app.listen(config.port);
 var io = require('socket.io').listen(app);
 
 // configuration ===============================================================
 app.use(morgan('dev'));
-app.set('view engine', 'ejs');
-
-// to get local files
-app.use(express.static(path.join(__dirname, 'public')));
 
 // routes ======================================================================
 require('./config/routes.js')(app, handlers);
