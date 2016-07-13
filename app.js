b2a1a8cd4f49c9c8c7a42806fdbcc04235c7762a
antonhansel <antoninribeaud@gmail.com>
2016-07-13 13:30:46 -0700

    try this way

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var handlers = require('./lib').handlers;
 var mongoose = require('mongoose');
 // Connect to mongoose
 mongoose.connect(config.mongoUrl);
+var allowHeaders = ['Accept', 'Accept-Version', 'Authorization', 'Content-Type', 'X-Requested-With', 'Session-Id'];
 
 app.listen(config.port);
 var io = require('socket.io').listen(app);
@@ -22,9 +23,15 @@ require('./config/routes.js')(app, handlers);
 require('./lib/socket-handler.js')(config, io);
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+  res.header('Access-Control-Allow-Headers', allowHeaders.join(', '));
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+
+  // Vary per origin, if headerS.origin is unset the cookie will not exist
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Vary', 'Origin');
+
+  return next();
 });
 
 console.log('Server listening on port ' + config.port);
